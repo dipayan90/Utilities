7eac3504ac462cb98bbab211d1db1bf46a64c662
dipayan90 <dipayan90@gmail.com>
2016-09-12 07:51:36 -0700

    Adding minimum subarray question. Implementation still to be done.

---
+++ Arrays/src/org/kajjoy/arrays/minimumSubArraySum/MinimumSubArraySum.java
@@ -0,0 +1,7 @@
+package org.kajjoy.arrays.minimumSubArraySum;
+
+/**
+ * Created by dipayan on 9/11/16.
+ */
+public class MinimumSubArraySum {
+}
---
+++ Arrays/src/org/kajjoy/arrays/minimumSubArraySum/MinimumSubArraySumRunner.java
@@ -0,0 +1,12 @@
+package org.kajjoy.arrays.minimumSubArraySum;
+
+/**
+ * Created by dipayan on 9/11/16.
+ *
+ * Given an array of n positive integers and a positive integer s, find the minimal length of a subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+
+ For example, given the array [2,3,1,2,4,3] and s = 7, the subarray [4,3] has the minimal length of 2 under the problem constraint.
+ */
+public class MinimumSubArraySumRunner {
+    // TODO: Implementation to be completed
+}
