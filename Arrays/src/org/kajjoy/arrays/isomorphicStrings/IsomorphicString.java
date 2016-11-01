83e2b315ccebc5d8ea3781f1f5c93340593e6659
Chattopadyay, Dipayan (DS) <Dipayan.Chattopadyay@cdk.com>
2016-11-01 14:17:29 -0700

    Updating the isomorphic streams program by fixing a bug.

--- Arrays/src/org/kajjoy/arrays/isomorphicStrings/IsomorphicString.java
+++ Arrays/src/org/kajjoy/arrays/isomorphicStrings/IsomorphicString.java
@@ -1,24 +1,37 @@
 package org.kajjoy.arrays.isomorphicStrings;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class IsomorphicString {
 
     protected static boolean check(String str1,String str2){
-        int[] tracker = new int[256];
+
         if(str1.length() != str2.length()){
             return false;
-        }else{
-            int len = str1.length();
-            int index = 0;
-            while(index < len){
-                if(tracker[str1.charAt(index)] == 0){
-                    tracker[(int)str1.charAt(index)] = (int) str2.charAt(index);
-                }else{
-                    if(tracker[(int)str1.charAt(index)] != (int) str2.charAt(index)){
-                        return false;
-                    }
+        }
+        Map<Character,Character> charMap = new HashMap<>();
+        int index = 0;
+        for(Character c : str1.toCharArray()){
+            Character actual = str2.charAt(index);
+            if(charMap.containsKey(c)){
+                Character expected = charMap.get(c);
+                if(expected != actual){
+                    return false;
                 }
-                index ++;
+            } else if(charMap.containsValue(actual)){
+                Character exp = charMap.entrySet().stream()
+                        .filter(e -> e.getValue().equals(actual))
+                        .map(Map.Entry::getKey)
+                        .findFirst()
+                        .orElse(null);
+                if(exp != c) {
+                    return false;
+                }
+            }else{
+                charMap.put(c,str2.charAt(index));
             }
+            index++;
         }
         return true;
     }
